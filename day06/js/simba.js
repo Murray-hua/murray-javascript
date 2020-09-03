7d825145e981c71ded48a8668ef24e96cbb8c6a2
murray <murrayhua@outlook.com>
2020-09-03 17:47:07 +0800

    1

--- day06/js/simba.js
+++ day06/js/simba.js
@@ -4,10 +4,6 @@ function repairZero(num) {
 }
 
 //闰年  被4整除且不能被100整除，或者直接被400整除
-function isLeapYear(year) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
-}
+let isLeapYear = year => year % 4 === 0 && year % 100 !== 0 || year % 400 === 0
 
-function reverseStr(str) {
-    return str.split('').reverse().join('')
-}
+let reverseStr =str =>str.split('').reverse().join('')
