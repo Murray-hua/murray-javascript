b2093c700680cfec72b35ea41539de6dee1a4872
murray <murrayhua@outlook.com>
2020-09-16 14:00:38 +0800

    1

---
+++ JavaScript/day13/murray.js
@@ -0,0 +1,24 @@
+let $ = {
+    throttle(callback, wait) {
+        let endTime = 0
+        return function () {
+            if (new Date() - endTime < wait) return; //console.log('too busy')
+            let res = callback.apply(this, arguments) // 防止this指错
+            endTime = new Date()
+            return res
+        }
+    },
+    trim(str) {
+        //如果是null或者undefind直接返回空字符串
+        return !str ? '' : str.trim()
+    },
+    repairZero: function (num){
+        return num < 10 ? ('0' + num): num
+    },
+    isLeapYear: function(year){
+        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    },
+    reverseStr: function(){
+        return str.split('').reverse().join('')
+    }
+}
