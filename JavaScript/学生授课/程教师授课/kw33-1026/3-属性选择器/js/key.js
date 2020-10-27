c5a0a793a7d27dabcfddc6db61209ea7f85037ba
Murray-hua <murrayhua@outlook.com>
2020-10-27 11:22:46 +0800

    1

---
+++ JavaScript/学生授课/程教师授课/kw33-1026/3-属性选择器/js/key.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zongjuan.wang on 2016/6/16.
+ */
+
+$(document).ready(function(){
+    //$("#news a[class]").css("background","#c9cbcb");  
+    //$("#news a[class='hot']").css("background","#c9cbcb");  
+    //$("#news a[class!='hot']").css("background","#c9cbcb");  
+    //$("#news a[href^='www']").css("background","#c9cbcb");  
+    //$("#news a[href$='html']").css("background","#c9cbcb"); 
+    $("#news a[href*='k2']").css("background","#c9cbcb");  
+})
